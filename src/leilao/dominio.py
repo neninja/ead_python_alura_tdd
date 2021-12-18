dfae626b451f031452d388e72a0c2309ad323ed0
Felipe Silva <felipe.dacs@gmail.com>
2021-12-18 08:56:44 -0300

    :tv: add aula 3

--- src/leilao/dominio.py
+++ src/leilao/dominio.py
@@ -1,5 +1,6 @@
 import sys
 
+
 class Usuario:
 
     def __init__(self, nome):
@@ -22,21 +23,17 @@ class Leilao:
     def __init__(self, descricao):
         self.descricao = descricao
         self.__lances = []
-
-    @property
-    def lances(self):
-        return self.__lances
-
-
-class Avaliador:
-
-    def __init__(self):
         self.maior_lance = sys.float_info.min
         self.menor_lance = sys.float_info.max
 
-    def avalia(self, leilao):
-        for lance in leilao.lances:
-            if lance.valor > self.maior_lance:
-                self.maior_lance = lance.valor
-            if lance.valor < self.menor_lance:
-                self.menor_lance = lance.valor
+    def propoe(self, lance: Lance):
+        if lance.valor > self.maior_lance:
+            self.maior_lance = lance.valor
+        if lance.valor < self.menor_lance:
+            self.menor_lance = lance.valor
+
+        self.__lances.append(lance)
+
+    @property
+    def lances(self):
+        return self.__lances[:]
--- src/leilao/principal.py
+++ src/leilao/principal.py
@@ -1,12 +1,12 @@
-from src.leilao.dominio import Leilao, Usuario, Lance, Avaliador
+from src.leilao.dominio import Usuario, Lance, Leilao, Avaliador
 
-gui = Usuario('gui')
-yuri = Usuario('yuri')
+gui = Usuario('Gui')
+yuri = Usuario('Yuri')
 
-lance_do_yuri = Lance(yuri, 100)
-lance_do_gui = Lance(gui, 150)
+lance_do_yuri = Lance(yuri, 100.0)
+lance_do_gui = Lance(gui, 150.0)
 
-leilao = Leilao("Celular")
+leilao = Leilao('Celular')
 
 leilao.lances.append(lance_do_yuri)
 leilao.lances.append(lance_do_gui)
@@ -14,10 +14,7 @@ leilao.lances.append(lance_do_gui)
 for lance in leilao.lances:
     print(f'O usuario {lance.usuario.nome} deu um lance de {lance.valor}')
 
-# restante do código omitido
-
 avaliador = Avaliador()
 avaliador.avalia(leilao)
 
-print(f'Maior lance: {avaliador.maior_lance}')
-print(f'Menor lance: {avaliador.menor_lance}')
+print(f'O menor lance foi de {avaliador.menor_lance} e o maior lance foi de {avaliador.maior_lance}')
